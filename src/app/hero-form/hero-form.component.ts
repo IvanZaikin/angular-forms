729b0f77c03d24c2bd7190f8945604dfa40b1f99
Ivan Zaikin <uazaikini@ksd.ua>
2017-11-22 13:50:19 +0200

    Added form validation

--- src/app/hero-form/hero-form.component.ts
+++ src/app/hero-form/hero-form.component.ts
@@ -23,6 +23,10 @@ export class HeroFormComponent implements OnInit {
 
   onSubmit() { this.submitted = true; }
 
+  newHero() {
+    this.model = new Hero(42, '', '');
+  }
+
   // TODO: Remove this when we're done
   get diagnostic() { return JSON.stringify(this.model); }
 
